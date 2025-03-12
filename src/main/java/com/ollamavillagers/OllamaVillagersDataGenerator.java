07ee1ca1fdfb318f60cd50930e8964250c2fcf8e
glugau <71564804+glugau@users.noreply.github.com>
2025-03-12 12:06:38 +0100

    First commit

---
+++ src/main/java/com/ollamavillagers/OllamaVillagersDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ollamavillagers;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OllamaVillagersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
